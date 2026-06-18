fdf1e8aad816c4ba1c456bb165b18a2d02df752b
Procid-GIT <Leo.Jiang@comcast.net>
2026-06-18 14:09:30 -0400

    modified backend.api for render deployment

--- Backend/backend_api.py
+++ Backend/backend_api.py
@@ -21,4 +21,5 @@ def get_ai_prediction():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
