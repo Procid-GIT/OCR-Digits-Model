b3579af61acc2af1630cb099ab4e93090ed02c57
Procid-GIT <Leo.Jiang@comcast.net>
2026-06-19 12:56:16 -0400

    changed ai_Image to aiImage

--- Backend/backend_api.py
+++ Backend/backend_api.py
@@ -21,5 +21,5 @@ def get_ai_prediction():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 7860))
     app.run(host="0.0.0.0", port=port, debug=True)
