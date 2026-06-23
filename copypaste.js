b10de89d9970541e5793ce3d511649aad4987d3c
Procid-GIT <Leo.Jiang@comcast.net>
2026-06-23 11:20:32 -0400

    added a copy paste button

---
+++ copypaste.js
@@ -0,0 +1,27 @@
+const cbutton = document.getElementById("copy");
+const output = document.getElementById("output");
+
+cbutton.addEventListener('click', copytoClipboard);
+
+async function copytoClipboard() {
+    const txt = output.innerText;
+
+    try{
+        await navigator.clipboard.writeText(txt);
+        const origin = cbutton.innerText;
+        cbutton.innerText = "Copied To Clipboard!";
+        cbutton.disabled = true;
+        
+        setTimeout(() => {
+            cbutton.innerText = origin;
+            cbutton.disabled = false;
+        }, 2000);
+
+    } catch (err) {
+        cbutton.innerText = "Failed to Copy!";
+        setTimeout(() => {
+            cbutton.innerText = origin;
+            cbutton.disabled = false;
+        }, 2000);
+    }
+}
